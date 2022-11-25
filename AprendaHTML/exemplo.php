d339778a3362d6d53fd5486284caa14235be975b
Andre R M Conforti <89820330+AndreConforti@users.noreply.github.com>
2022-11-25 17:38:45 -0300

    Add files via upload

---
+++ AprendaHTML/exemplo.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <form action="segunda.php" method="get">
+            Nome:
+            <input type="text" name="nome"/>
+            <br/>Idade:
+            <input type="text" name="idade"/>
+            <br/><input type="submit" value="ok"/>
+        </form>
+    </body>
+</html>
